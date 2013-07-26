5cba7a0c4231f324985a3a0e468af33a18427d69
Toder Svyatoslav <toderswat@gmail.com>
2013-07-26 20:44:41 +0300

    missing comments added

--- include/ServiceConfiguration.h
+++ include/ServiceConfiguration.h
@@ -23,12 +23,27 @@ namespace OAuth
          */
         std::string tokenExchangeUrl;
     public:
+        /**
+         * Sets field values according to provided data.
+         * @param tokenRequestUrl URL used to request temporary token
+         * @param authorizeUrl URL to which user will be redirected to authorize access
+         * @param tokenExchangeUrl URL used to exchange temporary token for permanent
+         */
         ServiceConfiguration(const std::string &tokenRequestUrl
             ,const std::string &authorizeUrl
             ,const std::string &tokenExchangeUrl);
 
+        /**
+         *  Returns URL used to request temporary token.
+         */
         const std::string &getAuthorizeUrl() const;
+        /**
+         *  Returns URL to which user will be redirected to authorize access.
+         */
         const std::string &getTokenRequestUrl() const;
+        /**
+         *  Returns URL used to exchange temporary token for permanent.
+         */
         const std::string &getTokenExchangeUrl() const;
     };
 }
