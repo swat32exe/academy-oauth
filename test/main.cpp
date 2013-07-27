3e399ab9b6431ab534f80dfb89f11529309f7226
Toder Svyatoslav <toderswat@gmail.com>
2013-07-27 19:43:26 +0300

    add google mock

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "HttpRequestTests.h"
 
 GTEST_API_ int main(int argc, char **argv)
 {
-    testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
