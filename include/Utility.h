5c768b2fa408bbfbe4161a1368938fc92a9a707b
Toder Svyatoslav <toderswat@gmail.com>
2013-07-27 23:23:44 +0300

    comments for urlEncode and urlDecode functions added

--- include/Utility.h
+++ include/Utility.h
@@ -7,7 +7,17 @@ namespace OAuth
 {
     namespace Utility
     {
+        /**
+         *  Performs percent encoding of string
+         *  @param data string to encode
+         *  @returns encoded string
+         */
         std::string urlEncode(std::string data);
+        /**
+         *  Decodes percent encoded string
+         *  @param data string to decode
+         *  @returns decoded string
+         */
         std::string urlDecode(std::string data);
     }
 }
