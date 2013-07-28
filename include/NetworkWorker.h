f00fec562a7a20d522dc125fd1201fb9f1053232
Toder Svyatoslav <toderswat@gmail.com>
2013-07-28 14:37:42 +0300

    fix comment

--- include/NetworkWorker.h
+++ include/NetworkWorker.h
@@ -17,7 +17,7 @@ namespace OAuth
         /**
          * Sends request and invokes listener when done
          * @param request request to make
-         * @listener listener.done() will be invoked when response received
+         * @param listener listener.done() will be invoked when response received
          */
         virtual void sendRequest(const HttpRequest &request, std::shared_ptr<NetworkDoneListener> listener) = 0;
         /**
