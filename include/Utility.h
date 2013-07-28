bafc95a42469b07288d39b8f0446fff8fafb63ad
Toder Svyatoslav <toderswat@gmail.com>
2013-07-28 14:15:25 +0300

    add utility functions
    hostFromUrl, resourceFromUrl, toString added
    toString needed because of gcc bug 52015

--- include/Utility.h
+++ include/Utility.h
@@ -2,6 +2,7 @@
 #define __UTILITY_H
 
 #include <string>
+#include <sstream>
 
 namespace OAuth
 {
@@ -19,6 +20,29 @@ namespace OAuth
          *  @returns decoded string
          */
         std::string urlDecode(std::string data);
+
+        /**
+         * Extracts host from url
+         * @param url url, something like www.example.com/test
+         * @returns host, something like www.example.com
+         */
+        std::string hostFromUrl(std::string url);
+        /**
+         * Extracts host from url
+         * @param url url, something like www.example.com/test
+         * @returns host, something like /test
+         */
+        std::string resourceFromUrl(std::string url);
+        /**
+         * Converts long long to string. Needed because of gcc bug:
+         * Bug 52015 - std::to_string does not work under MinGW
+         */
+        template<typename T> std::string toString(T x)
+        {
+            std::stringstream stringStream;
+            stringStream<<x;
+            return stringStream.str();
+        }
     }
 }
 
