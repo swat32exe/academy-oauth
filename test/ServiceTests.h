092dd32eab612480cef08d519559510323d2b9dc
Toder Svyatoslav <toderswat@gmail.com>
2013-08-07 01:29:49 +0300

    Add tests for credentials exchange and request signing

--- test/ServiceTests.h
+++ test/ServiceTests.h
@@ -55,4 +55,60 @@ namespace OAuth
         OAuth::Service service(configuration, sendRequest);
         ASSERT_EQ("http://example.com/auth?some=parameter&oauth_token=tokenkey", service.getAuthorizeUrl(token));
     }
+
+    TEST_F(ServiceTests, credentials_exchange_hmac_sha1)
+    {
+        OAuth::ServiceConfiguration configuration("http://term.ie/oauth/example/request_token.php"
+            ,"http://example.com/auth"
+            ,"http://term.ie/oauth/example/access_token.php"
+            ,"key"
+            ,"secret"
+            ,"http://example.com/callback"
+            ,HMAC_SHA1);
+        OAuth::Service service(configuration, sendRequest);
+        std::future<Token> futureToken = service.exchangeToken(Token("requestkey", "requestsecret"), "verifier");
+        Token token = futureToken.get();
+        ASSERT_EQ("accesskey", token.getToken());
+        ASSERT_EQ("accesssecret", token.getSecret());
+    }
+
+    TEST_F(ServiceTests, sign_request_get_hmac_sha1)
+    {
+        OAuth::ServiceConfiguration configuration("http://term.ie/oauth/example/request_token.php"
+            ,"http://example.com/auth"
+            ,"http://term.ie/oauth/example/access_token.php"
+            ,"key"
+            ,"secret"
+            ,"http://example.com/callback"
+            ,HMAC_SHA1);
+        OAuth::Service service(configuration, sendRequest);
+
+        HttpRequest request(GET, "http://term.ie/oauth/example/echo_api.php");
+        request.addHeader("Some-Wierd-Header", "some_data");
+        request.addQueryParameter("testParameter","value");
+        request.addQueryParameter("anotherParameter","anotherValue");
+        service.signRequest(request, Token("accesskey", "accesssecret"));
+        std::string response = sendRequest(request);
+        ASSERT_EQ("testParameter=value&anotherParameter=anotherValue", response);
+    }
+
+    TEST_F(ServiceTests, sign_request_post_hmac_sha1)
+    {
+        OAuth::ServiceConfiguration configuration("http://term.ie/oauth/example/request_token.php"
+            ,"http://example.com/auth"
+            ,"http://term.ie/oauth/example/access_token.php"
+            ,"key"
+            ,"secret"
+            ,"http://example.com/callback"
+            ,HMAC_SHA1);
+        OAuth::Service service(configuration, sendRequest);
+
+        HttpRequest request(POST, "http://term.ie/oauth/example/echo_api.php");
+        request.addHeader("Some-Wierd-Header", "some_data");
+        request.addBodyParameter("testParameter","value");
+        request.addBodyParameter("anotherParameter","anotherValue");
+        service.signRequest(request, Token("accesskey", "accesssecret"));
+        std::string response = sendRequest(request);
+        ASSERT_EQ("testParameter=value&anotherParameter=anotherValue", response);
+    }
 }
