8cdd5e4a79efef1958013f13548188426bb8ae87
Toder Svyatoslav <toderswat@gmail.com>
2013-08-17 19:21:42 +0300

    Add and partially implement OAuth 2.0 Service
    add Service class
    implement Service constructors and getAuthorizeUrl method
    move ServiceConfiguration header to include

---
+++ include/oauth2/Service.h
@@ -0,0 +1,79 @@
+#ifndef O2SERVICE_H_
+#define O2SERVICE_H_
+
+#include <string>
+#include <functional>
+#include <future>
+
+#include "ServiceConfiguration.h"
+
+namespace OAuth
+{
+    class HttpRequest;
+}
+
+namespace OAuth2
+{
+    class Token;
+
+    typedef std::function<std::string(const OAuth::HttpRequest&)> send_request_t;
+
+    class Service
+    {
+        static const std::string RESPONSE_TYPE;
+        static const std::string CLIENT_ID;
+        static const std::string REDIRECT_URI;
+        static const std::string SCOPE;
+        static const std::string STATE;
+
+    public:
+        /**
+         *  Create Service with specified configuration.
+         *  @param configuration Configuration, that will be used.
+         *  @param sendRequest used to make requests over network.
+         *  Must send provided request and return response. May throw exceptions.
+         */
+        Service(const ServiceConfiguration &configuration, const send_request_t &sendRequest);
+
+        /**
+         *  Create Service with specified configuration.
+         *  Uses cURL as default for work with network.
+         *  @param configuration Configuration, that will be used.
+         */
+        Service(const ServiceConfiguration &configuration);
+
+        /**
+         *  Get url, to which user should be redirected.
+         *  @param state State string, that will be added to url.
+         *  If state is empty, it will not be added.
+         */
+        std::string getAuthorizeUrl(const std::string &state = "") const;
+
+        /**
+         *  Get access token using way, specified by configuration.
+         *  @param url Url, at which user was redirected back (if applicable).
+         */
+        std::future<Token> getAccessToken(const std::string &url = "") const;
+
+        /*
+         *  Sign HttpRequest using token.
+         *  @param request Request to sign.
+         *  @param token Token, used to sign request.
+         */
+        void signRequest(OAuth::HttpRequest &request, const Token &token) const;
+
+        /**
+         *  Refresh access token.
+         *  @param expiredToken Old token.
+         */
+        std::future<Token> refreshAccessToken(const Token &expiredToken) const;
+
+    private:
+        ServiceConfiguration configuration;
+        send_request_t sendRequest;
+
+        std::string makeRedirectUrlParameters(const std::string &state, const std::string &responseType) const;
+    };
+}
+
+#endif
---
+++ src/oauth2/Service.cpp
@@ -0,0 +1,63 @@
+#include <oauth2/Service.h>
+
+#include <string>
+#include <stdexcept>
+
+#include <utility/Url.h>
+#include <DefaultSendRequest.h>
+
+namespace OAuth2
+{
+    const std::string Service::RESPONSE_TYPE = "response_type";
+    const std::string Service::CLIENT_ID = "client_id";
+    const std::string Service::REDIRECT_URI = "redirect_uri";
+    const std::string Service::SCOPE = "scope";
+    const std::string Service::STATE = "state";
+
+    Service::Service(const ServiceConfiguration &configuration, const send_request_t &sendRequest) :
+        configuration(configuration)
+        ,sendRequest(sendRequest)
+    {
+    }
+
+    Service::Service(const ServiceConfiguration &configuration) :
+        configuration(configuration)
+        ,sendRequest(defaultSendRequest)
+    {
+    }
+
+    std::string Service::getAuthorizeUrl(const std::string &state) const
+    {
+        std::string authorizeUrl = configuration.getAuthEndpoint();
+
+        if(authorizeUrl.find("?") == std::string::npos)
+            authorizeUrl += "?";
+        else
+            authorizeUrl += "&";
+
+        switch (configuration.getGrantType()) {
+        case AUTH_CODE_GRANT:
+        case IMPLICIT_GRANT:
+            authorizeUrl += makeRedirectUrlParameters(state,  configuration.getResponseType());
+            break;
+        default:
+            throw std::logic_error("getAuthorizeUrl does not support specified grant type");
+        }
+
+        return authorizeUrl;
+    }
+
+    std::string Service::makeRedirectUrlParameters(const std::string &state,
+            const std::string &responseType) const
+    {
+        std::string parameters =
+            RESPONSE_TYPE + "=" + OAuth::Utility::urlEncode(responseType)
+            + "&" + CLIENT_ID + "=" + OAuth::Utility::urlEncode(configuration.getClientId())
+            + "&" + REDIRECT_URI + "=" + OAuth::Utility::urlEncode(configuration.getRedirectUri());
+        if(!configuration.getScope().empty())
+            parameters += "&" + SCOPE + "=" + OAuth::Utility::urlEncode(configuration.getScope());
+        if(!state.empty())
+            parameters += "&" + STATE + "=" + OAuth::Utility::urlEncode(state);
+        return parameters;
+    }
+}
