3e562ba6b20b35e653e9317dcffed72dff35a77d
Toder Svyatoslav <toderswat@gmail.com>
2013-08-18 14:04:22 +0300

    Add test for OAuth 2.0 Service signRequest method

--- test/oauth2/ServiceTests.h
+++ test/oauth2/ServiceTests.h
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
 #include <oauth2/Service.h>
+#include <oauth2/Token.h>
+#include <HttpRequest.h>
 
 namespace OAuth2
 {
@@ -49,5 +51,14 @@ namespace OAuth2
             "https://server.example.com/authorize?response_type=token&client_id=s6BhdRkqt3"
             "&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb&state=xyz",
             service.getAuthorizeUrl("xyz"));
+    }
+
+    TEST_F(ServiceTests2, sign_request)
+    {
+        Service service(testConfigurationImplicitGrant1);
+        Token token("mF_9.B5f-4.1JqM");
+        OAuth::HttpRequest httpRequest(OAuth::POST, "https://data.example.com/");
+        service.signRequest(httpRequest, token);
+        ASSERT_EQ("Bearer mF_9.B5f-4.1JqM", httpRequest.getHeaders().find("Authorization")->second);
     }
 }
