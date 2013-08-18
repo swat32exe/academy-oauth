9c938304dc71b294102baf76f957a59213d58ce7
Melzas <sfdsrc@gmail.com>
2013-08-18 21:09:53 +0300

    Update Service tests to use ServiceBuilder

--- test/oauth2/ServiceTests.h
+++ test/oauth2/ServiceTests.h
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <oauth2/Service.h>
+#include <oauth2/ServiceBuilder.h>
 #include <oauth2/Token.h>
 #include <HttpRequest.h>
 
@@ -9,35 +10,27 @@ namespace OAuth2
     class ServiceTests2: public testing::Test
     {
     public:
-        ServiceConfiguration testConfigurationCodeGrant1;
-        ServiceConfiguration testConfigurationImplicitGrant1;
+        ServiceBuilder authCodeGrantBuilder;
+        ServiceBuilder implicitGrantBuilder;
 
-        ServiceTests2() :
-            testConfigurationCodeGrant1(AUTH_CODE_GRANT,
-                    "https://server.example.com/authorize",
-                    "https://server.example.com/token",
-                    "s6BhdRkqt3",
-                    "https://client.example.com/cb",
-                    "",
-                    "johndoe",
-                    "A3ddj3w"
-            )
-            , testConfigurationImplicitGrant1(IMPLICIT_GRANT,
-                    "https://server.example.com/authorize",
-                    "https://server.example.com/token",
-                    "s6BhdRkqt3",
-                    "https://client.example.com/cb",
-                    "",
-                    "johndoe",
-                    "A3ddj3w"
-            )
+        ServiceTests2()
         {
+            authCodeGrantBuilder.setGrantType(AUTH_CODE_GRANT)
+                    .setAuthEndpoint("https://server.example.com/authorize")
+                    .setTokenEndpoint("https://server.example.com/token")
+                    .setClientId("s6BhdRkqt3")
+                    .setRedirectUri("https://client.example.com/cb");
+            implicitGrantBuilder.setGrantType(IMPLICIT_GRANT)
+                    .setAuthEndpoint("https://server.example.com/authorize")
+                    .setTokenEndpoint("https://server.example.com/token")
+                    .setClientId("s6BhdRkqt3")
+                    .setRedirectUri("https://client.example.com/cb");
         }
     };
 
     TEST_F(ServiceTests2, get_authorize_url_no_parameters_auth_code_grant)
     {
-        Service service(testConfigurationCodeGrant1);
+        Service service = authCodeGrantBuilder.build();
         ASSERT_EQ(
                 "https://server.example.com/authorize?response_type=code&client_id=s6BhdRkqt3"
                 "&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb&state=xyz",
@@ -46,7 +39,7 @@ namespace OAuth2
 
     TEST_F(ServiceTests2, get_authorize_url_no_parameters_implicit_grant)
     {
-        Service service(testConfigurationImplicitGrant1);
+        Service service = implicitGrantBuilder.build();
         ASSERT_EQ(
             "https://server.example.com/authorize?response_type=token&client_id=s6BhdRkqt3"
             "&redirect_uri=https%3A%2F%2Fclient.example.com%2Fcb&state=xyz",
@@ -55,7 +48,7 @@ namespace OAuth2
 
     TEST_F(ServiceTests2, sign_request)
     {
-        Service service(testConfigurationImplicitGrant1);
+        Service service = implicitGrantBuilder.build();
         Token token("mF_9.B5f-4.1JqM");
         OAuth::HttpRequest httpRequest(OAuth::POST, "https://data.example.com/");
         service.signRequest(httpRequest, token);
