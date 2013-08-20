a20e572df697e1a7c963770ccff4b787d37dcca3
Toder Svyatoslav <toderswat@gmail.com>
2013-08-20 12:41:00 +0300

    Rename function, replace magic constant, remove atoi
    rename queryParametersFromUrl to extractQueryParameters
    '?' in extractQueryParameters function replaced by ParameterList::QUERY_SEPARATOR
    use stringstream instead of atoi

--- src/utility/Url.h
+++ src/utility/Url.h
@@ -38,7 +38,7 @@ namespace OAuth
          * @param url Url with parameters, e.g. http://www.example.com/test?q1=f.
          * @returns Parameters, e.g. q1=f
          */
-        std::string queryParametersFromUrl(const std::string &url);
+        std::string extractQueryParameters(const std::string &url);
         /**
          * Normalizes url
          * e.g http://www.example.com, www.example.com/, www.example.com
